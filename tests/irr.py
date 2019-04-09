cad670f1fba370c3c41d1e8043a05424e1dd26be
pvankessel <pvankessel@pewresearch.org>
2019-04-09 16:34:04 -0400

    added unit tests for new irr scoring functions

--- tests/irr.py
+++ tests/irr.py
@@ -5,7 +5,13 @@ import unittest
 class IRRTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        import pandas as pd
+        self.dataset = pd.DataFrame({
+            "code": [1, 0, 1, 1, 0, 1, 0, 1, 0, 0],
+            "coder": [1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+            "weight": [.5, 1.5, 1.0, .75, .175, .5, 1.5, 1.0, .75, .175],
+            "doc": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+        })
 
     def test_kappa_sample_size_power(self):
         from pewanalytics.stats.irr import kappa_sample_size_power
@@ -20,6 +26,31 @@ class IRRTests(unittest.TestCase):
         self.assertTrue(sample_size==140)
         sample_size = kappa_sample_size_CI(.8, .7, .5, kappaU=.9, alpha=.05)
         self.assertTrue(sample_size==197)
+
+    def test_compute_scores(self):
+        from pewanalytics.stats.irr import compute_scores
+        scores = compute_scores(self.dataset, 1, 2, "code", "doc", "coder", "weight", pos_label=1)
+        self.assertAlmostEquals(scores['roc_auc'], .83, 2)
+        self.assertAlmostEquals(scores['matthews_corrcoef'], .68, 2)
+        self.assertAlmostEquals(scores['alpha_unweighted'], .64, 2)
+        self.assertAlmostEquals(scores['f1'], .8, 2)
+        self.assertAlmostEquals(scores['recall'], .67, 2)
+        self.assertAlmostEquals(scores['coder1_std'], .24, 2)
+        self.assertAlmostEquals(scores['coder2_std'], .24, 2)
+        self.assertAlmostEquals(scores['precision'], 1.0, 2)
+        self.assertAlmostEquals(scores['n'], 5, 2)
+        self.assertAlmostEquals(scores['cohens_kappa'], .63, 2)
+        self.assertAlmostEquals(scores['precision_recall_min'], .67, 2)
+        self.assertAlmostEquals(scores['coder2_mean'], .4, 2)
+        self.assertAlmostEquals(scores['coder1_mean'], .6, 2)
+        self.assertAlmostEquals(scores['pct_agree_unweighted'], .8, 2)
+        self.assertAlmostEquals(scores['accuracy'], .81, 2)
+
+    def test_compute_overall_scores(self):
+        from pewanalytics.stats.irr import compute_overall_scores
+        scores = compute_overall_scores(self.dataset, "doc", "code", "coder")
+        self.assertAlmostEquals(scores['alpha'], .64, 2)
+        self.assertAlmostEquals(scores['fleiss_kappa'], .6, 2)
 
     def tearDown(self):
         pass
