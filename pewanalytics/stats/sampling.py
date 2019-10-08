d73aa9ece1a2f7d945d17b9624310feb06619bf8
pvankessel <pvankessel@pewresearch.org>
2019-10-08 10:57:24 -0400

    updated documentation and improved unit testing; also got rid of the alternative stratification implementation (there's no reason to have two equivalent functions)

--- pewanalytics/stats/sampling.py
+++ pewanalytics/stats/sampling.py
@@ -11,10 +11,15 @@ import pandas as pd
 def compute_sample_weights_from_frame(frame, sample, weight_vars):
 
     """
-    :frame: DataFrame
-    :sample: Number of dimensions you want
-    :weight_vars:
-    :return:
+    Takes two dataframes and computes sampling weights for the second one, based on the first. The first dataframe
+    should be equivalent to the population that the second dataframe, a sample, was drawn from. Weights will be
+    calculated based on the differences in the distribution of one or more variables specified in `weight_vars`
+    (these should be the names of columns). Returns a series equal in length to the `sample` with the computed weights.
+
+    :param frame: DataFrame (must contain all of the columns specified in `weight_vars`)
+    :param sample: DataFrame (must contain all of the columns specified in `weight_vars`)
+    :param weight_vars: The names of the columns to use when computing weights.
+    :return: A Series containing the weights for each row in the `sample`
     """
 
     if len(weight_vars) > 0:
@@ -50,8 +55,13 @@ def compute_sample_weights_from_frame(frame, sample, weight_vars):
 def compute_balanced_sample_weights(sample, weight_vars, weight_column=None):
 
     """
-    :sample: Number of dimensions you want
-    :weight_vars:
+    Takes a DataFrame and one or more column names (`weight_vars`) and computes weights such that every unique
+    combination of values in the weighting columns are balanced (when weighted, the sum of the observations with each
+    combination will be equal to one another.)
+
+    :param sample: DataFrame (must contain all of the columns specified in `weight_vars`)
+    :param weight_vars: The names of the columns to use when computing weights.
+    :param weight_column: An option column containing existing weights, which can be factored into the new weights.
     :return:
     """
 
@@ -68,7 +78,6 @@ def compute_balanced_sample_weights(sample, weight_vars, weight_column=None):
                 ]
             )
         )
-        # for combo in itertools.product([True, False], repeat=len(weight_vars)):
         for combo in combos:
             if weight_column:
                 combo_weights[combo] = float(
@@ -124,18 +133,29 @@ class SampleExtractor(object):
         sampling_strategy="random",
         stratify_by=None,
         id_col=None,
-        logger=None,
+        verbose=False,
         seed=None,
     ):
 
         """
-        :param df: dataframe
-        :param sample_size:  integer (for sample size) or decimal (percentage of df)
-        :param stratify_by: (column or list of columns in the dataframe to stratify on)
-        :param id_col: column in the dataframe to be used as the record id
-        :param seed: random seed ( optional )
-        :param type: type of stratification to use . pass "even" if sample evenly from each strata. Otherwise strata-proportional sampling will be applied.
-        :return:
+        A helper class for extracting samples using various sampling methods. After specifying the sampling
+        options on the SampleExtractor, you can make multiple calls to `extract`, passing in a DataFrame from
+        which to sample, and the desired size of the sample. The available sampling methods are:
+
+        - all: Returns all of the IDs
+        - random: Returns a random sample
+        - stratify: Proportional stratification, method from Kish, Leslie. "Survey sampling." (1965). Chapter 4.
+        - stratify_even: Sample evenly from each strata (will obviously not be representative)
+        - stratify_guaranteed: Proportional stratification, but the sample is guaranteed to contain at least one
+        observation from each strata (if sample size is small and/or there are many small strata, the resulting sample
+        may be far from representative)
+
+        :param sampling_strategy: The method to be used to extract samples. Options are: all, random, stratify,
+        stratify_even, stratify_guaranteed
+        :param stratify_by: Optional name of a column or list of columns in the DataFrame to stratify on
+        :param id_col: column in the DataFrame to be used as the unique ID of observations
+        :param verbose: Whether or not to print information during the sampling process (default=False)
+        :param seed: Random seed (optional)
         """
 
         strategies = [
@@ -144,7 +164,6 @@ class SampleExtractor(object):
             "stratify",
             "stratify_even",
             "stratify_guaranteed",
-            "stratify_alt",
         ]
         if sampling_strategy not in strategies:
             raise Exception(
@@ -157,118 +176,121 @@ class SampleExtractor(object):
         self.id_col = id_col
         self.seed = seed
         self.sampling_strategy = sampling_strategy
-        self.logger = logger
+        self.verbose = verbose
 
         if not self.seed:
             self.seed = int(round(1000 * np.random.random()))
 
     def extract(self, df, sample_size):
 
-        if self.sampling_strategy == "all":
+        """
+        Extract a sample from a DataFrame
 
+        :param df: The DataFrame to sample from
+        :param sample_size: The desired size of the sample
+        :return: A list of IDs reflecting the observations selected from the DataFrame during sampling
+        """
+
+        doc_ids = None
+
+        if self.sampling_strategy == "all":
             doc_ids = df[self.id_col].values
 
         elif self.sampling_strategy == "random":
-
-            # If no stratification at all
-            if self.logger:
-                self.logger.info("Basic random sample")
-            doc_ids = self._basic_sample(df[self.id_col], sample_size).values
+            doc_ids = self._random_sample(df[self.id_col], sample_size).values
 
         elif self.sampling_strategy.startswith("stratify"):
 
-            if self.logger:
+            if self.verbose:
                 print("Stratify on columns: {}".format(",".join(self.stratify_by)))
+
             df["_stratify_by"] = df[self.stratify_by].astype(str).apply("".join, axis=1)
-            frame_size = df.shape[0]
+
             # So you can pass in a decimal proportion of total dataframe or number of samples
             sample_n = (
                 sample_size
                 if sample_size >= 1
-                else int(round(sample_size * frame_size))
+                else int(round(sample_size * df.shape[0]))
             )
 
             if self.sampling_strategy == "stratify":
-
-                doc_ids = self._stratify_sample_final(df, sample_n)
+                doc_ids = self._stratify_sample(df, sample_n)
 
             elif self.sampling_strategy == "stratify_even":
-
-                doc_ids = self._stratify_sample_even(df, sample_n)
+                doc_ids = self._stratify_even_sample(df, sample_n)
 
             elif self.sampling_strategy == "stratify_guaranteed":
+                doc_ids = self._stratify_guaranteed_sample(df, sample_n)
 
-                strata_one = self._take_one_per_strata(df, sample_n)
-                left_to_sample = sample_n - len(strata_one)
-                if left_to_sample > 0:
-                    doc_ids = self._stratify_sample_final(
-                        df[~df[self.id_col].isin(strata_one)], left_to_sample
-                    )
-                    doc_ids = list(doc_ids) + list(strata_one)
-                else:
-                    print("Nothing left to sample, no stratification applied")
-                    doc_ids = strata_one
-
-            elif self.sampling_strategy == "stratify_alt":
-
-                doc_ids = self._stratify_sample_alt(df, sample_n)
-
-        if "_stratify_by" in df.columns:
             del df["_stratify_by"]
 
-        print("Sample of %i extracted" % (len(doc_ids)))
+        if self.verbose:
+            print("Sample of %i extracted" % (len(doc_ids)))
 
         return list(doc_ids)
 
-    def _take_one_per_strata(self, df, sample_n):
+    def _random_sample(self, df, sample_size):
 
-        # Number of groups to stratify by must be less than the total sample size
-        strata_groups = df.groupby("_stratify_by")[self.id_col].count().count()
-        if sample_n > strata_groups:
-            print(
-                "Sampling one document per strata first ({} strata total)".format(
-                    strata_groups
-                )
-            )
-            one_per = (
-                df.groupby("_stratify_by")
-                .apply(lambda x: x.sample(1, random_state=self.seed))[self.id_col]
-                .values
-            )
-            return one_per
+        if self.verbose:
+            print("Basic random sample")
+        if sample_size >= 1:
+            try:
+                return df.sample(int(sample_size), random_state=self.seed)
+            except ValueError:
+                return df
         else:
-            print(
-                "There are more strata groups ({}) than things to sample: {}".format(
-                    strata_groups, sample_n
-                )
-            )
-            one_per = (
-                df.groupby("_stratify_by")
-                .apply(lambda x: x.sample(1, random_state=self.seed))[self.id_col]
-                .sample(sample_n)
-                .values
-            )
-            return one_per
+            return df.sample(frac=float(sample_size), random_state=self.seed)
 
-    def _stratify_sample_even(self, df, sample_size):
+    def _stratify_sample(self, df, sample_n):
 
-        """
-        Sample evenly from each group that's stratified by.
-        At least one document per group
+        if self.verbose:
+            print("Kish-style stratification")
 
-        :param df:
-        :param sample_size:
-        """
+        # Subset & copy cols that we care about
+        data = df.copy()[[self.id_col] + ["_stratify_by"]]
+        frame_size = data.shape[0]
+
+        # Shuffle the dataframe
+        if self.verbose:
+            print("Random seed: {}".format(self.seed))
+        np.random.seed(self.seed)
+        if self.verbose:
+            print("Dataframe before sorting: {}".format(data.head()))
+        data.index = np.random.permutation(data.index)
+
+        # Re-sort grouped by strata
+        data = data.groupby("_stratify_by").apply(lambda x: x.sort_index())
+        data.index = list(range(0, frame_size))
+        if self.verbose:
+            print("Dataframe after shuffle & groupby sorting: {}".format(data.head()))
+
+        skip_interval = float(frame_size) / float(sample_n)
+
+        start_index = np.random.uniform(0, skip_interval)  # index to start from
+        if self.verbose:
+            print("Start index: {}".format(start_index))
+        sample_index = np.round(
+            (np.zeros(sample_n) + start_index) + (np.arange(sample_n) * skip_interval)
+        )
+
+        # Return the real id column
+        sample_ids = data[data.index.isin(sample_index)][self.id_col].values
+
+        return sample_ids
+
+    def _stratify_even_sample(self, df, sample_size):
+
         random.seed(self.seed)
         docs_per_strata = int(
             float(sample_size)
             / float(df.groupby("_stratify_by")[self.id_col].count().count())
         )
-        print(
-            "Drawing even samples of {} across all stratification groups".format(
-                docs_per_strata
+        if self.verbose:
+            print(
+                "Drawing even samples of {} across all stratification groups".format(
+                    docs_per_strata
+                )
             )
-        )
         doc_ids = []
         for strata in df["_stratify_by"].unique():
             strata_data = df[df["_stratify_by"] == strata]
@@ -282,109 +304,45 @@ class SampleExtractor(object):
 
         return doc_ids
 
-    def _stratify_sample_final(self, df, sample_n):
+    def _stratify_guaranteed_sample(self, df, sample_size):
 
-        """
-            Proportional stratification
-
-            Method sourced from : Kish, Leslie. "Survey sampling." (1965).  Chapter 4.
-
-        :param df: dataframe
-        :param sample_n:  integer (for sample size)
-        :return: list
-        """
-        print("Kish-style stratification")
-        # Subset & copy cols that we care about
-        data = df.copy()[[self.id_col] + ["_stratify_by"]]
-        frame_size = data.shape[0]
-
-        # Shuffle the dataframe
-        if self.logger:
-            self.logger.debug("Random seed: {}".format(self.seed))
-        np.random.seed(self.seed)
-        if self.logger:
-            self.logger.debug("Dataframe before sorting:{}".format(data.head()))
-        data.index = np.random.permutation(data.index)
-        # Re-sort grouped by strata
-        data = data.groupby("_stratify_by").apply(lambda x: x.sort_index())
-        data.index = list(range(0, frame_size))
-        if self.logger:
-            self.logger.debug(
-                "Dataframe after shuffle & groupby sorting:{}".format(data.head())
+        # Number of groups to stratify by must be less than the total sample size
+        strata_groups = df.groupby("_stratify_by")[self.id_col].count().count()
+        if sample_size > strata_groups:
+            if self.verbose:
+                print(
+                    "Sampling one document per strata first ({} strata total)".format(
+                        strata_groups
+                    )
+                )
+            strata_one = (
+                df.groupby("_stratify_by")
+                .apply(lambda x: x.sample(1, random_state=self.seed))[self.id_col]
+                .values
+            )
+        else:
+            if self.verbose:
+                print(
+                    "There are more strata groups ({}) than things to sample: {}".format(
+                        strata_groups, sample_size
+                    )
+                )
+            strata_one = (
+                df.groupby("_stratify_by")
+                .apply(lambda x: x.sample(1, random_state=self.seed))[self.id_col]
+                .sample(sample_size)
+                .values
             )
 
-        skip_interval = float(frame_size) / float(sample_n)
-
-        start_index = np.random.uniform(0, skip_interval)  # index to start from
-        if self.logger:
-            self.logger.info("start index : {}".format(start_index))
-        mysample_index = np.round(
-            (np.zeros(sample_n) + start_index) + (np.arange(sample_n) * skip_interval)
-        )
-
-        # Return the real id column
-        mysample_id = data[data.index.isin(mysample_index)][self.id_col].values
-
-        return mysample_id
-
-    def _stratify_sample_alt(self, df, sample_size):
-        """
-        In stratified sampling, the overall proportion of each group is preserved
-
-        :param df: dataframe to sample from
-        :param sample_size: sample size ( int )
-        :param stratify_by: column name to stratify by ( or list of columns )
-        :param id_col:
-        :return: id column for the sample & stats about the sample
-        """
-        print("New stratification method")
-        docs_per_strata = {}
-        if "_stratify_by":
-
-            def group_percentages(df, groupcols, id_col=None):
-
-                """
-                Returns percentages (summing to 1) of each group out of total
-
-                :param df: data frame
-                :param groupcols: list of columns
-                :param id_col: string, column name to stratify on
-                """
-
-                if not id_col:
-                    id_col = df.index
-
-                return pd.pivot_table(df, id_col, groupcols, aggfunc=[len]).apply(
-                    lambda x: x / x.sum()
-                )["len"]
-
-            strata_proportions = group_percentages(df, "_stratify_by", self.id_col)[
-                self.id_col
-            ]
-            docs_per_strata = (strata_proportions * sample_size).round().to_dict()
-
-            doc_ids = []
-            for strata, strata_data in df.groupby("_stratify_by"):
-                sample_strata_ids = self._basic_sample(
-                    strata_data[self.id_col], docs_per_strata[strata]
-                ).values.tolist()
-                doc_ids = doc_ids + sample_strata_ids
-
-            print("Stratified sample of %i extracted" % (len(doc_ids)))
-
+        left_to_sample = sample_size - len(strata_one)
+        if left_to_sample > 0:
+            doc_ids = self._stratify_sample(
+                df[~df[self.id_col].isin(strata_one)], left_to_sample
+            )
+            doc_ids = list(doc_ids) + list(strata_one)
         else:
-            doc_ids = self._basic_sample(
-                df[self.id_col], sample_size, seed=self.seed
-            ).values.tolist()
+            if self.verbose:
+                print("Nothing left to sample, no stratification applied")
+            doc_ids = strata_one
 
         return doc_ids
-
-    def _basic_sample(self, df, size):
-
-        if size >= 1:
-            try:
-                return df.sample(int(size), random_state=self.seed)
-            except ValueError:
-                return df
-        else:
-            return df.sample(frac=float(size), random_state=self.seed)
--- tests/sampling.py
+++ tests/sampling.py
@@ -11,27 +11,34 @@ class SamplingTests(unittest.TestCase):
         self.df["id"] = self.df.index
         self.df["flag"] = self.df["text"].map(lambda x: "disney" in x).astype(int)
         self.df["flag2"] = self.df["text"].map(lambda x: "princess" in x).astype(int)
+        self.df["flag3"] = 0
+        self.df.loc[self.df.index == 0, "flag3"] = 1
         self.oversample = pd.concat(
             [self.df[self.df["flag"] == 1.0][:50], self.df[self.df["flag"] == 0.0][:50]]
         )
         self.sample = self.df[:100]
+        self.frame_props = (self.df.groupby(["flag", "flag2"]).count() / len(self.df))[
+            "id"
+        ].to_dict()
 
     def test_compute_sample_weights_from_frame(self):
         from pewanalytics.stats.sampling import compute_sample_weights_from_frame
 
         weights = compute_sample_weights_from_frame(self.df, self.oversample, ["flag"])
         weights = sorted([round(w, 3) for w in weights.unique()])
-        self.assertTrue([0.096, 1.904] == weights)
+        self.assertEqual([0.096, 1.904], weights)
 
     def test_compute_balanced_sample_weights(self):
         from pewanalytics.stats.sampling import compute_balanced_sample_weights
 
         weights = compute_balanced_sample_weights(self.sample, ["flag"])
+        self.sample["temp"] = weights
         weights = sorted([round(w, 3) for w in weights.unique()])
-        self.assertTrue([0.515, 16.667] == weights)
+        self.assertEqual([0.515, 16.667], weights)
+        for val in self.sample.groupby("flag").sum()["temp"].values:
+            self.assertAlmostEqual(val, 50, 1)
 
-    def test_sample_extractor(self):
-
+    def test_sample_extractor_all(self):
         import numpy as np
         from pewanalytics.stats.sampling import SampleExtractor
 
@@ -40,54 +47,20 @@ class SamplingTests(unittest.TestCase):
         )
         self.assertTrue(len(all) == len(self.df))
 
+    def test_sample_extractor_random(self):
+        import numpy as np
+        from pewanalytics.stats.sampling import SampleExtractor
+
         random = SampleExtractor(
             id_col="id", seed=42, sampling_strategy="random"
         ).extract(self.df, 100)
-        self.assertTrue(len(random) == 100)
-        self.assertTrue(self.df.loc[random]["flag"].mean() < self.df["flag"].mean() * 2)
+        self.assertEqual(len(random), 100)
+        self.assertEqual(self.df.loc[random]["flag"].mean(), 0.03)
+        self.assertEqual(self.df["flag"].mean(), 0.048)
 
-        frame_props = (self.df.groupby(["flag", "flag2"]).count() / len(self.df))[
-            "id"
-        ].to_dict()
-        guaranteed = SampleExtractor(
-            id_col="id",
-            seed=42,
-            sampling_strategy="stratify_guaranteed",
-            stratify_by=["flag", "flag2"],
-        ).extract(self.df, 100)
-        self.assertTrue(len(guaranteed) == 100)
-        sample_props = (
-            self.df.loc[guaranteed].groupby(["flag", "flag2"]).count() / len(guaranteed)
-        )["id"].to_dict()
-        for k, v in frame_props.items():
-            self.assertTrue(k in sample_props.keys())
-            self.assertTrue(sample_props[k] > 0)
-            self.assertTrue(sample_props[k] < frame_props[k] + 0.03)
-            self.assertTrue(sample_props[k] > frame_props[k] - 0.03)
-
-        guaranteed = SampleExtractor(
-            id_col="id",
-            seed=42,
-            sampling_strategy="stratify_guaranteed",
-            stratify_by=["flag", "flag2"],
-        ).extract(self.df, 4)
-        self.assertTrue(len(guaranteed) == 4)
-        sample_props = (
-            self.df.loc[guaranteed].groupby(["flag", "flag2"]).count() / len(guaranteed)
-        )["id"].to_dict()
-        self.assertTrue(np.average(list(sample_props.values())) == 0.25)
-
-        even = SampleExtractor(
-            id_col="id",
-            seed=42,
-            sampling_strategy="stratify_even",
-            stratify_by=["flag", "flag2"],
-        ).extract(self.df, 20)
-        self.assertTrue(len(even) == 20)
-        sample_props = (
-            self.df.loc[even].groupby(["flag", "flag2"]).count() / len(even)
-        )["id"].to_dict()
-        self.assertTrue(np.average(list(sample_props.values())) == 0.25)
+    def test_sample_extractor_stratify(self):
+        import numpy as np
+        from pewanalytics.stats.sampling import SampleExtractor
 
         normal_strat = SampleExtractor(
             id_col="id",
@@ -100,27 +73,83 @@ class SamplingTests(unittest.TestCase):
             self.df.loc[normal_strat].groupby(["flag", "flag2"]).count()
             / len(normal_strat)
         )["id"].to_dict()
-        for k, v in sample_props.items():
-            self.assertTrue(sample_props[k] > 0)
-            self.assertTrue(sample_props[k] < frame_props[k] + 0.01)
-            self.assertTrue(sample_props[k] > frame_props[k] - 0.01)
+        for k, v in self.frame_props.items():
+            self.assertTrue(k in sample_props.keys())
+            self.assertGreater(sample_props[k], 0)
+        self.assertEqual(sample_props[(0, 1)], 0.02)
+        self.assertEqual(sample_props[(1, 0)], 0.04)
+        self.assertEqual(sample_props[(0, 0)], 0.93)
+        self.assertEqual(sample_props[(1, 1)], 0.01)
 
-        strat_alt = SampleExtractor(
+        normal_strat = SampleExtractor(
             id_col="id",
             seed=42,
-            stratify_by=["flag", "flag2"],
-            sampling_strategy="stratify_alt",
+            stratify_by=["flag", "flag2", "flag3"],
+            sampling_strategy="stratify",
         ).extract(self.df, 100)
         self.assertTrue(len(normal_strat) == 100)
         sample_props = (
-            self.df.loc[strat_alt].groupby(["flag", "flag2"]).count() / len(strat_alt)
+            self.df.loc[normal_strat].groupby(["flag", "flag2", "flag3"]).count()
+            / len(normal_strat)
         )["id"].to_dict()
-        for k, v in sample_props.items():
-            self.assertTrue(sample_props[k] > 0)
-            self.assertTrue(sample_props[k] < frame_props[k] + 0.01)
-            self.assertTrue(sample_props[k] > frame_props[k] - 0.01)
+        for val1, val2, val3 in sample_props.keys():
+            self.assertNotEqual(val3, 1)
+        self.assertEqual(sample_props[(1, 0, 0)], 0.04)
+        self.assertEqual(sample_props[(1, 1, 0)], 0.01)
+        self.assertEqual(sample_props[(0, 1, 0)], 0.02)
+        self.assertEqual(sample_props[(0, 0, 0)], 0.93)
 
-        # TODO: tests for random seeds
+    def test_sample_extractor_stratify_even(self):
+        import numpy as np
+        from pewanalytics.stats.sampling import SampleExtractor
+
+        even = SampleExtractor(
+            id_col="id",
+            seed=42,
+            sampling_strategy="stratify_even",
+            stratify_by=["flag", "flag2"],
+        ).extract(self.df, 20)
+        self.assertEqual(len(even), 20)
+        sample_props = (
+            self.df.loc[even].groupby(["flag", "flag2"]).count() / len(even)
+        )["id"].to_dict()
+        for val in sample_props.values():
+            self.assertEqual(val, 0.25)
+
+    def test_sample_extractor_stratify_guaranteed(self):
+        import numpy as np
+        from pewanalytics.stats.sampling import SampleExtractor
+
+        guaranteed = SampleExtractor(
+            id_col="id",
+            seed=42,
+            sampling_strategy="stratify_guaranteed",
+            stratify_by=["flag", "flag2"],
+        ).extract(self.df, 100)
+        self.assertEqual(len(guaranteed), 100)
+        sample_props = (
+            self.df.loc[guaranteed].groupby(["flag", "flag2"]).count() / len(guaranteed)
+        )["id"].to_dict()
+        for k, v in self.frame_props.items():
+            self.assertTrue(k in sample_props.keys())
+            self.assertGreater(sample_props[k], 0)
+        self.assertEqual(sample_props[(0, 1)], 0.03)
+        self.assertEqual(sample_props[(1, 0)], 0.05)
+        self.assertEqual(sample_props[(0, 0)], 0.91)
+        self.assertEqual(sample_props[(1, 1)], 0.01)
+
+        guaranteed = SampleExtractor(
+            id_col="id",
+            seed=42,
+            sampling_strategy="stratify_guaranteed",
+            stratify_by=["flag", "flag2"],
+        ).extract(self.df, 4)
+        self.assertEqual(len(guaranteed), 4)
+        sample_props = (
+            self.df.loc[guaranteed].groupby(["flag", "flag2"]).count() / len(guaranteed)
+        )["id"].to_dict()
+        for val in sample_props.values():
+            self.assertEqual(val, 0.25)
 
     def tearDown(self):
         pass
