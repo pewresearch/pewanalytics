b5346f1617be5d9a4bff52481313a2c83fb8eaf9
pvankessel <pvankessel@pewresearch.org>
2020-06-08 15:03:13 -0400

    trying another css override method

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -100,9 +100,9 @@ html_theme_options = {"navigation_depth": 3}
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_context = {
-    "css_files": ["_static/theme_overrides.css"]  # override wide tables in RTD theme
-}
+# html_context = {
+#     "css_files": ["_static/theme_overrides.css"]  # override wide tables in RTD theme
+# }
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
